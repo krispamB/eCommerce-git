89198466c25295d7376e6adfbb5cc2a175a8a1d7
Christopher <chrystopam@gmail.com>
2022-09-27 14:23:15 +0100

    Order schema correction

--- models/Order.js
+++ models/Order.js
@@ -10,13 +10,13 @@ const orderSchema = Schema(
       ref: 'User',
     },
     orderItems: [
-      { name: { type: String, required: true } },
-      { qty: { type: Number, required: true } },
-      { image: { type: String, required: true } },
-      { price: { type: Number, required: true } },
       {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
         product: {
-          type: Schema.Types.ObjectId,
+          type: mongoose.Schema.Types.ObjectId,
           required: true,
           ref: 'Product',
         },
